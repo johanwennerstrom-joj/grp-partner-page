7a39112cb6446f582042f58bb4c99b3ac6b63e4d
johanwennerstrom-joj <johan.wennerstrom@hyperisland.se>
2020-04-22 19:41:58 +0200

    added instructions for git practice

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h2>Hey, David made a pull request </h2>
+    <h2>
+      Try putting something here just for git practice if you need it! And if i
+      think you have behaved well i will merge it!
+    </h2>
   </Layout>
 )
 
