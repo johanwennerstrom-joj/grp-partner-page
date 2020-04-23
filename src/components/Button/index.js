512eedcda3e344cc0ca7eb9ecc6fdda4c08c3fee
johanwennerstrom-joj <johan.wennerstrom@hyperisland.se>
2020-04-23 12:07:32 +0200

    Added onClick to the button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react"
 
-const Button = () => {
-    return (
-        <button>Become a partner</button>
-    )
+const Button = props => {
+  // Added a onClick as a prop that will take the prop "clicked" when rendered. This is so we can put the logic for this in index.js instead of here
+
+  return <button onClick={props.clicked}>Become a partner</button>
 }
 
-export default Button;
+export default Button
