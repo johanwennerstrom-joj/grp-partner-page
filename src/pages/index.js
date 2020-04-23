6f8bcdd79855a54559f2cfda30b24bed200213d2
johanwennerstrom-joj <johan.wennerstrom@hyperisland.se>
2020-04-23 12:11:48 +0200

    added a function to the 'clicked'in pages/ index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
-import Button from '../components/Button/index'
+import Button from "../components/Button/index"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h2>Hey, David made a pull request </h2>
-    <h2>
-      Try putting something here just for git practice if you need it! And if i
-      think you have behaved well i will merge it!
-    </h2>
-    <Button />
-  </Layout>
-)
+const IndexPage = () => {
+  // Added a test function to the click for demonstrational purposes
+  const handleClick = () => {
+    alert("Button was clicked")
+  }
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h2>Hey, David made a pull request </h2>
+      <h2>
+        Try putting something here just for git practice if you need it! And if
+        i think you have behaved well i will merge it!
+      </h2>
+      <Button clicked={handleClick} />
+    </Layout>
+  )
+}
 
 export default IndexPage
