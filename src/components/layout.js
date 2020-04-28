e0fbddd7218da0c4a4ce42c807263c7e2294eed2
johanwennerstrom-joj <johan.wennerstrom@hyperisland.se>
2020-04-28 15:40:55 +0200

    Removed import that wasn't needed and added comments

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,22 @@
 import React from "react"
+
+// Component imports, try to keep all the imports here and keep it tidy! :)
 import NavBar from "./NavBar/index"
 import Header from "./Header/index"
 import Button from "./Button/index"
-import { useStaticQuery, graphql } from "gatsby"
 
 const Layout = ({ children }) => {
   return (
     <>
-
       <NavBar />
       <main>{children}</main>
-=======
+
       <main>
         {" "}
         <NavBar />
         <Header button={<Button />} />
         {children}
       </main>
-
     </>
   )
 }
