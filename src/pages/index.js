92d9d1326399807483b3307dd8976f6243922325
ArlindPestisha <arlind.pestisha@hyperisland.se>
2020-04-30 10:03:42 +0200

    Importing the CardContanier
    Importing the CardContanier and wraping the PartnerCard(chilldren).

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,8 @@ import Data from "../data/partners.json"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PartnerCard from "../components/PartnerCard/index"
+import CardContainer from "../components/Card contanier/index"
+
 
 const IndexPage = () => {
   //state, setQuery changes state. We will use it with the buttonrow
@@ -46,18 +48,20 @@ const IndexPage = () => {
         website="www.zurich.com"
       /> */}
 
-      {test.map(item => {
-        return (
-          <PartnerCard
-            type={item.type}
-            theme={item.themes}
-            hq={item.hq}
-            workingRegion={item.workingRegion}
-            website={item.website}
-            key={item.id}
-          />
-        )
-      })}
+      <CardContainer>
+        {partners.map(item => {
+          return (
+            <PartnerCard
+              type={item.type}
+              theme={item.themes}
+              hq={item.hq}
+              workingRegion={item.workingRegion}
+              website={item.website}
+              key={item.id}
+            />
+          )
+        })}
+      </CardContainer>
     </Layout>
   )
 }
