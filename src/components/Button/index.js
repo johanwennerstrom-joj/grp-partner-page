5bb4135b528a60f14e842a471c3682d309f91053
dvdherron <dvdherron@gmail.com>
2020-05-04 15:39:29 +0200

    [refs #00000] update main header
    fixed the core spacing of header in widescreen.
    
    the "join us" link is sitting in the right place now.
    
    also updated the button component so that it is more flexible in regards to adding classNames and routes

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 
 // Css import
 import "./index.scss"
@@ -8,11 +8,9 @@ const Button = props => {
   // Added a onClick as a prop that will take the prop "clicked" when rendered. This is so we can put the logic for this in index.js instead of here
   //<button onClick={props.clicked}>Become a partner</button>
   return (
-    <button> Join Us </button>
-
-    // <Link to="/joinus">
-    //   <button>Become a partner</button>
-    // </Link>
+    <Link className={props.className} to={props.route}>
+      {props.text}
+    </Link>
   )
 }
 
