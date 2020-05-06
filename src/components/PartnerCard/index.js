1671a4129684992069ff91232bd49d8c56ed6ff0
dvdherron <dvdherron@gmail.com>
2020-05-06 09:50:32 +0200

    [refs #00000] Center grid
    * Made the cards slightly smaller so that the fit better on the grid
    
    * added margins to the grid so that it sits in the middle of the page. woohoo!

--- src/components/PartnerCard/index.js
+++ src/components/PartnerCard/index.js
@@ -7,29 +7,31 @@ const PartnerCard = props => {
       <article className="card">
         <div className="card__img--wrapper">
           <img
+            alt={" "}
             className="card__logo"
             loading="lazy"
             src={props.image}
-            alt={props.alt}
           />
         </div>
-        <h3>{props.organisation}</h3>
-        <p>
-          <span className="card__title">Type:</span> {props.type}
-        </p>
-        <p>
-          <span className="card__title">Theme:</span> {props.theme}
-        </p>
-        <p>
-          <span className="card__title">Headquarters:</span> {props.hq}
-        </p>
-        <p>
-          <span className="card__title">Working Region:</span>{" "}
-          {props.workingRegion}
-        </p>
-        <a href={props.website} target="_blank" rel="noopener noreferrer">
-          <span className="card__title">WEBSITE</span>
-        </a>
+        <div className="card__content-container">
+          <h3>{props.organisation}</h3>
+          <p>
+            <span className="card__title">Type:</span> {props.type}
+          </p>
+          <p>
+            <span className="card__title">Theme:</span> {props.theme}
+          </p>
+          <p>
+            <span className="card__title">Headquarters:</span> {props.hq}
+          </p>
+          <p>
+            <span className="card__title">Working Region:</span>{" "}
+            {props.workingRegion}
+          </p>
+          <a href={props.website} target="_blank" rel="noopener noreferrer">
+            <span className="card__title">WEBSITE</span>
+          </a>
+        </div>
       </article>
     </>
   )
