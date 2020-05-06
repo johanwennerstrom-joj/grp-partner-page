ce3b7f93b244be7f20f086f87cdead797bf1bc4a
quena <quena.u.s@gmail.com>
2020-05-06 11:45:46 +0200

    removed useState because it was not used

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 
 // Data
 import Data from "../data/partners.json"
@@ -63,33 +63,33 @@ const IndexPage = e => {
       <CardContainer>
         {filtered === " "
           ? partners.map(partner => {
-              return (
-                <PartnerCard
-                  image={partner.image}
-                  organisation={partner.organisation}
-                  type={partner.type}
-                  theme={partner.themes}
-                  hq={partner.hq}
-                  workingRegion={partner.workingRegion}
-                  website={partner.website}
-                  key={partner._id}
-                />
-              )
-            })
+            return (
+              <PartnerCard
+                image={partner.image}
+                organisation={partner.organisation}
+                type={partner.type}
+                theme={partner.themes}
+                hq={partner.hq}
+                workingRegion={partner.workingRegion}
+                website={partner.website}
+                key={partner._id}
+              />
+            )
+          })
           : filtered.map(partner => {
-              return (
-                <PartnerCard
-                  image={partner.image}
-                  organisation={partner.organisation}
-                  type={partner.type}
-                  theme={partner.themes}
-                  hq={partner.hq}
-                  workingRegion={partner.workingRegion}
-                  website={partner.website}
-                  key={partner._id}
-                />
-              )
-            })}
+            return (
+              <PartnerCard
+                image={partner.image}
+                organisation={partner.organisation}
+                type={partner.type}
+                theme={partner.themes}
+                hq={partner.hq}
+                workingRegion={partner.workingRegion}
+                website={partner.website}
+                key={partner._id}
+              />
+            )
+          })}
       </CardContainer>
     </Layout>
   )
